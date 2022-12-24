83e8e993517fccfd94bc0106b223e089426c7f8e
syahril syahputra <syahril.dev@gmail.com>
2022-12-24 22:16:58 +0800

    add crud people

--- server/src/controllers/peopleController.js
+++ server/src/controllers/peopleController.js
@@ -9,4 +9,40 @@ const get = async (req, res) => {
     res.json({ message: error });
   }
 };
-module.exports = { get };
+const add = async (req, res) => {
+  // console.log(req.body)
+  try {
+    const query = `insert into people (name, gender, parent) value ('${req.body.name}', '${req.body.gender}', ${req.body.parent})`;
+    req.db.query(query, function (err, data) {
+      if (err) throw err;
+
+      // console.log(data)
+      res.json(data);
+    });
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+const edit = async (req, res) => {
+  try {
+    const query = `update people set name='${req.body.name}', gender='${req.body.gender}', parent=${req.body.parent} where id=${req.params.id}`;
+    req.db.query(query, function (err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+const del = async (req, res) => {
+  try {
+    const query = `delete from people where id=${req.params.id}`;
+    req.db.query(query, function (err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+module.exports = { get, add, edit, del };
--- server/src/routes/people.js
+++ server/src/routes/people.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const peopleController = require("../controllers/peopleController");
 
 router.get("/", peopleController.get);
+router.post("/", peopleController.add);
+router.put("/:id", peopleController.edit);
+router.delete("/:id", peopleController.del);
 
 module.exports = router;
